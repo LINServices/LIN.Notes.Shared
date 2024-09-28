f8f0428af8fd4b13cb2df24a830363819cfd5397
Alexander Giraldo <giraldojhong4@gmail.com>
2024-09-28 15:58:10 -0500

    first commit

---
+++ Modals/DeleteModal.razor.cs
@@ -0,0 +1,46 @@
+﻿namespace LIN.Notes.Shared.Modals;
+
+public partial class DeleteModal
+{
+
+
+    [Parameter]
+    public string Content { get; set; }
+
+
+    /// <summary>
+    /// Acción al presionar sobre el botón de editar.
+    /// </summary>
+    [Parameter]
+    public Action OnAccept { get; set; } = () => { };
+
+
+    [Parameter]
+    public Action OnCancel { get; set; } = () => { };
+
+
+
+    /// <summary>
+    /// Key.
+    /// </summary>
+    private string Key { get; init; } = Guid.NewGuid().ToString();
+
+
+
+
+    /// <summary>
+    /// Abrir el modal.
+    /// </summary>
+    public void Show()
+    {
+        _ = InvokeAsync(() =>
+        {
+            StateHasChanged();
+            Js.InvokeVoidAsync("showModal", $"popup-modal-{Key}", DotNetObjectReference.Create(this), $"btn-accept-{Key}", $"btn-cancel-{Key}", $"btn-close-{Key}");
+
+        });
+
+    }
+
+
+}
---
+++ usings.cs
@@ -0,0 +1,3 @@
+﻿global using LIN.Types.Notes.Models;
+global using Microsoft.AspNetCore.Components;
+global using Microsoft.JSInterop;
